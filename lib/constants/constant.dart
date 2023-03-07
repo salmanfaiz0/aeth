5e64d1555631f316251e6fe70245f3e52f328c9a
salman faiz <18ncs20@meaec.edu.in>
2023-03-07 21:48:35 +0530

    interview

---
+++ lib/constants/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Consttext {
+  static const String info =
+      "1: Click the add symbol button\n2: Enter your title and description \n3: Click the add button\n4: Display the items\n5: Swipe left or right for delete items";
+
+  static const String headinfo = "Todo Guide";
+}
